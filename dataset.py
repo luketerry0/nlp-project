57014e863880a675215d1464b3435eab89fc23f8
Luke Terry <luke.terry0682@gmail.com>
2025-04-22 17:33:09 -0500

    make huggingface dataset

--- dataset.py
+++ dataset.py
@@ -3,13 +3,14 @@ This file contains a custom torch dataset which we can use to load the data
 """
 
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import numpy as np
+from datasets import Dataset
 
 class RiddleDataset(Dataset):
     """Dataset for the problem set"""
 
-    def __init__(self, sp=True, train=True, root_dir="/home/cs529321/nlp-project/data/", transform=None):
+    def __init__(self, sp=True, train=True, root_dir="./data/", transform=None):
         dataset = "SP" if sp else "WP"
         if train:
             self.data = np.load(root_dir+dataset+"-train.npy", allow_pickle=True)
@@ -38,8 +39,33 @@ class CustomTransform:
             formatted_question += ' [SEP] ' + sample['choice_list'][i]
 
         return (formatted_question, sample['label'])
+    
+# get the pytorch dataset as a generator in order to make a huggingface dataset with it
+def get_huggingface_dataset(train = True, sp=True, root_dir="./data/"):
+    dataset = "SP" if sp else "WP"
+    if train:
+        data = np.load(root_dir+dataset+"-train.npy", allow_pickle=True)
+    else:
+        data = np.load(root_dir+dataset+"_eval_data_for_practice.npy", allow_pickle=True)
+        
+    def data_gen(data):
+        # you need to manually set the type of all features or errors happen
+        for i in range(0, len(data)):
+            yield {
+                'id': str(data[i]['id']),
+                'question': str(data[i]['question']),
+                'label': str(data[i]['label']),
+                'choice_list': [str(choice) for choice in data[i]['choice_list']],
+                'label': int(data[i]['label'])
+                }
+
+    ds = Dataset.from_generator(data_gen, gen_kwargs={"data": data})
+    return ds
+    
 
 if __name__ == "__main__":
-    test = RiddleDataset(transform=CustomTransform())
-    print(len(test))
-    print(test[0])
+    # ds = RiddleDataset(transform=CustomTransform())
+    # print(len(ds))
+    # print(ds[0])
+
+    huggingface_dataset = get_huggingface_dataset()
